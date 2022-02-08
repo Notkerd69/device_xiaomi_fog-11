b274a1a02e8c1293f507cc0e84cf11809ca25283
hpnightowl <iamhp2k@gmail.com>
2022-02-09 05:27:48 +0530

    fog: Drop audio.a2dp.default
     * Building from here:
       https://github.com/LineageOS/android_build/blob/lineage-18.1/target/product/full_base.mk#L35
    
    Change-Id: Ib71c1abd381468ec3621d60e5ca959f738f51168

--- device.mk
+++ device.mk
@@ -52,7 +52,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@6.0-impl \
     android.hardware.audio.service \
     android.hardware.bluetooth.audio@2.0-impl \
-    audio.a2dp.default \
     audio.bluetooth.default \
     audio.primary.bengal \
     audio.primary.default \
