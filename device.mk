5b5e5452d43255c10f7bb6e944c1067495c9418b
KakatkarAkshay <akshayakakatkaraa@gmail.com>
2022-05-08 01:22:59 +0400

    fog: Import sunfish powerhint.json from android-12.1.0_r5
    Change-Id: I552870c130ceaac4d20322acf6d1da22ad5017ec
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
    Signed-off-by: alternoegraha <37102951+alternoegraha@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -363,6 +363,9 @@ PRODUCT_PACKAGES += \
     android.hardware.power-service.xiaomi-libperfmgr \
     android.hardware.power.stats@1.0-service.mock
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json
+
 # Public libraries
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/public.libraries.txt:$(TARGET_COPY_OUT_VENDOR)/etc/public.libraries.txt
