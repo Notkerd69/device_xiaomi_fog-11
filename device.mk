4a95c0f8bd344ce06ca719e162ba4e95dc5787a9
sashascurtu <sasha.scurtu@gmail.com>
2023-12-17 23:45:22 -0500

    fog: Build sensorservice 1.0 HAL to vendor
    Fixes:
    W HidlServiceManagement: Waited one second for android.frameworks.sensorservice@1.0::ISensorManager/default
    I hwservicemanager: Since android.frameworks.sensorservice@1.0::ISensorManager/default is not registered, trying to start it as a lazy HAL.
    I HidlServiceManagement: getService: Trying again for android.frameworks.sensorservice@1.0::ISensorManager/default...
    W libc    : Unable to set property ctl.interface_start to android.frameworks.sensorservice@1.0::ISensorManager/default: error code: 0x20
    
    Change-Id: If6fvdef9dc39daaba9138fe3f8acf06cb6a0e78f8

--- device.mk
+++ device.mk
@@ -416,6 +416,7 @@ PRODUCT_PACKAGES += \
 # Sensors
 PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
+    android.frameworks.sensorservice@1.0.vendor \
     android.hardware.sensors@2.1-service.xiaomi_bengal-multihal \
     libsensorndkbridge
 
