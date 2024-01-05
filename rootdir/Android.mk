Eagle-Projekt/device_xiaomi_fog

fog: rootdir: Adapt for libperfmgr
Co-authored-by: Ramii Ahmed <ramy@ahmedramy.com>
Change-Id: Ia5f59d183056728dc2966b99ff74dbb4fcc83c7b
Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>
Signed-off-by: Tanmsyffa <sltnmsyffaa@gmail.com>

configs/rootdir/Android.mk
@@ -84,6 +84,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.power.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.power.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := ueventd.qcom.rc
 LOCAL_MODULE_STEM  := ueventd.rc
 LOCAL_MODULE_TAGS  := optional

Notkerd69/device_xiaomi_fog-11

Signed-off-by: alternoegraha <37102951+alternoegraha@users.noreply.github.com>

rootdir/Android.mk
@@ -84,6 +84,14 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+LOCAL_MODULE       := init.qcom.power.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.qcom.power.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
+include $(BUILD_PREBUILT)
+
+include $(CLEAR_VARS)
 LOCAL_MODULE       := init.qcom.usb.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC