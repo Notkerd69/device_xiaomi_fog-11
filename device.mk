4703f93e09ff1957143e852762e51fc0f5803421
Pranav Vashi <neobuddy89@gmail.com>
2024-01-11 19:56:57 +0000

    fog: Imports parts from sm6150-common
    - Also remove refresh rate and haptic feedback level adjustment because we don't support that
    
    Signed-off-by: Notkerd69 <29781314+Notkerd69@users.noreply.github.com>

--- device.mk
+++ device.mk
@@ -137,6 +137,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     disable_configstore
 
+# Device-specific settings
+PRODUCT_PACKAGES += \
+    XiaomiParts
+
 # Device ID
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.device_id_attestation.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.device_id_attestation.xml
